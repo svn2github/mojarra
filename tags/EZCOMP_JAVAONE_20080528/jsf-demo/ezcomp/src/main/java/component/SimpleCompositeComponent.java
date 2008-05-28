9b5b8f774a6ff6ad04607eff6cd7abf1654b5573
edburns <edburns@761efcf2-d34d-6b61-9145-99dcacc3edf1>
2008-05-28 19:58:36 +0000

    snapshot
    git-svn-id: https://svn.java.net/svn/mojarra~svn@4832 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ tags/EZCOMP_JAVAONE_20080528/jsf-demo/ezcomp/src/main/java/component/SimpleCompositeComponent.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package component;
+
+import javax.faces.component.UINamingContainer;
+
+/**
+ *
+ * @author edburns
+ */
+public class SimpleCompositeComponent extends UINamingContainer {
+
+    @Override
+    public String getFamily() {
+        return "SimpleCompositeComponent";
+    }
+    
+}
